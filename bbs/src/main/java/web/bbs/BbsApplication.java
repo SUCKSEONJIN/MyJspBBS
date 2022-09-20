26a5ede925df90d431c9b3da26e233dc7aa5399e
SUCKSEONJIN <wlq862@naver.com>
2022-09-20 23:06:57 +0900

    membmer info view making

--- bbs/src/main/java/web/bbs/BbsApplication.java
+++ bbs/src/main/java/web/bbs/BbsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @Import(WebMybatisConfig.class)
-//@Import(TestConfig.class)
 @SpringBootApplication
 public class BbsApplication {
 
